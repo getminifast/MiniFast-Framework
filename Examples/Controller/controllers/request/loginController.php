5cff29d0259f5687344b43c32f84c52b7a198782
getminifast <contact@getminifast.org>
2018-09-24 16:45:59 +0200

    No more example on GitHub, see docs

--- Examples/Controller/controllers/request/loginController.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$view = new View('templates');
-$view->render('index');
--- Examples/Controller/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once './../../src/class/autoload.php';
-
-$controller = new Controller('controllers');
-$controller->useController('RequestLogin'); // RequestLogin is the controller in controllers/request/loginController.php
