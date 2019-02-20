2030850eadd33a300533a120a8861f1b5df8b7ea
getminifast <contact@getminifast.org>
2019-02-20 16:47:18 +0100

    Add ArrayAccess and Countable Iterators

--- src/Core/Iterable.php
+++ src/Core/Iterable.php
@@ -2,7 +2,7 @@
 
 namespace MiniFast\Core\Route;
 
-class SectionIterator implements SeekableIterator
+class SectionIterator implements SeekableIterator, ArrayAccess, Countable
 {
     private $elems = [];
     private $key = 0;
@@ -72,5 +72,51 @@ class SectionIterator implements SeekableIterator
     public function valid()
     {
         return isset($this->elems[$this->key]);
+    }
+
+    /**
+     * Test if the key exists
+     * @param int $key The key
+     */
+    public function offsetExists($key)
+    {
+        return isset($this->elems[$key]);
+    }
+
+    /**
+     * Return the element at $key
+     * @param int $key The key
+     */
+    public function offsetGet($key)
+    {
+        return $this->elems[$key];
+    }
+
+    /**
+     * Set $value at $key offset
+     * @param int   $key   The key
+     * @param mixed $value The value to set
+     */
+    public function offsetSet($key, $value)
+    {
+        $this->elems[$key] = $value;
+    }
+
+    /**
+     * Unset offset at $key
+     * @param int $key The key
+     */
+    public function offsetUnset($key)
+    {
+        unset($this->elems[$key]);
+    }
+
+    /**
+     * Return the number of elements
+     * @return int The number of elements
+     */
+    public function count()
+    {
+        return count($this->elems);
     }
 }
