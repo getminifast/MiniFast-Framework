c586478e4b83ab0c6f08211a2dd6315beaa7d2df
getminifast <contact@getminifast.org>
2019-02-21 14:06:13 +0100

    Add namepsaces to const

--- src/Config.php
+++ src/Config.php
@@ -4,18 +4,22 @@ namespace MiniFast;
 
 class Config
 {
+    const ROUTE_NAMESPACE = '\\MiniFast\\Core\\Route\\';
+
     const ROUTER_DEFAULT_INDEX = 'default';
 
-    const ROUTER_ROUTE_CLASSNAME = 'Route';
-    const ROUTER_ROUTEITERATOR_CLASSNAME = 'RouteIterator';
+    const ROUTER_ROUTE_CLASSNAME = self::ROUTE_NAMESPACE . 'Route';
+    const ROUTER_ROUTEITERATOR_CLASSNAME = self::ROUTE_NAMESPACE
+        . 'RouteIterator';
     const ROUTER_ROUTES_INDEX = 'routes';
     const ROUTER_ROUTES_MODELS = 'model';
     const ROUTER_ROUTES_NAME_INDEX = 'name';
     const ROUTER_ROUTES_RESPONSE = 'response';
     const ROUTER_ROUTES_VIEW = 'view';
 
-    const ROUTER_SECTION_CLASSNAME = 'Section';
-    const ROUTER_SECTIONITERATOR_CLASSNAME = 'SectionIterator';
+    const ROUTER_SECTION_CLASSNAME = self::ROUTE_NAMESPACE . 'Section';
+    const ROUTER_SECTIONITERATOR_CLASSNAME = self::ROUTE_NAMESPACE
+        . 'SectionIterator';
     const ROUTER_SECTIONS_INDEX = 'sections';
     const ROUTER_SECTIONS_NAME = 'name';
 }
