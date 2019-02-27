88794de153e3880f8ac8dc5830a2e7f5d75b699a
getminifast <contact@getminifast.org>
2019-02-27 13:50:58 +0100

    Fix missing method call

--- src/Core/Route/Controller.php
+++ src/Core/Route/Controller.php
@@ -14,6 +14,7 @@ class Controller
 
     public function __construct()
     {
+        $this->setUrl();
         $this->route = $this->section = null;
         $this->default = new Route(
             [
